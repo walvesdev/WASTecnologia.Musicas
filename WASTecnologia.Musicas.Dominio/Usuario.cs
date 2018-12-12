c3e096914326c462c376620cb6241c48972c06a1
Willian <Willian@DESKTOP-D9VNGIP>
2018-12-12 13:59:17 -0200

    Atualização Pacotes nuget e instalação Identity

---
+++ WASTecnologia.Musicas.Dominio/Usuario.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WASTecnologia.Musicas.Dominio
+{
+    public class Usuario
+    {
+        [Required(ErrorMessage = "O Email é obrigatorio")]
+        [MaxLength(30, ErrorMessage ="o Email não pode ter mais de 30 caracteres!")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "A senha é obrigatoria")]
+        [DataType(DataType.Password)]
+        public string Senha { get; set; }
+    }
+}
---
+++ WASTecnologia.Musicas.Web/Controllers/UsuariosController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WASTecnologia.Musicas.Dominio;
+
+namespace WASTecnologia.Musicas.Web.Controllers
+{
+    public class UsuariosController : Controller
+    {
+        // GET: Usuarios
+        public ActionResult CriarUsuario()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult CriarUsuario(Usuario usuario)
+        {
+            if (ModelState.IsValid)
+            {
+                 
+            }
+            return View(usuario);
+        }
+    }
+}
